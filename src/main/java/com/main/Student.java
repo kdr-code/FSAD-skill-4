0a5180d4d9b51d26257a9efa56f4264bb841630c
kdr-code <kdr.anandini@gmail.com>
2026-03-10 15:14:53 +0530

    changes in Student

--- src/main/java/com/main/Student.java
+++ src/main/java/com/main/Student.java
@@ -23,6 +23,22 @@ public class Student {
         this.year = year;
     }
 
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     public void display() {
         System.out.println(studentId + " " + name + " " + course + " " + year);
     }
